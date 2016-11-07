8f0ecb28aa518abc6edcf14a011f2555d16b49c7
Ty <ty.clark@bayefficiency.com>
2016-11-07 14:57:41 -0800

    adding photo and drop downs

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,9 +1,27 @@
 class PropertiesController < ApplicationController
 	
 	def index
-		@properties = Properties.all
+		@properties = Property.all
 	end
 
 	def edit
+		@property = Property.find(params[:id])
 	end
+
+	def update
+		@property = Property.find(params[:id])
+
+		if @property.update(property_params)
+			redirect_to action: "index"
+		else
+			render 'edit'
+		end
+	end
+
+
+private
+  def property_params
+    params.require(:property).permit(:name, :address, :city, :state, :zip, :photo, :loan_type, :purchase_price, :down_payment_pct, 
+    	:annual_gross_rental_income, :stated_residence, :actual_residence, :investor_id)
+  end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,33 @@
 class Property < ActiveRecord::Base
+	enum loan_type: [:commercial, :residential, :cash_out_residential]
+	enum stated_residence: [:existing_property, :new_property, :rental_unit]
+	enum actual_residence: [:existing_home, :new_home, :rental_home]
+
+	validates :name, :address, :city, :state, :zip, :loan_type, :stated_residence, :actual_residence, presence: true
+	validates :purchase_price, :down_payment_pct, :annual_gross_rental_income, :investor_id, 
+		numericality: { only_decimal: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100000000}
+
+
+  	has_attached_file :photo, :styles => { :small => "150x150", :medium => "500x500"  },
+                      :url  => "/ttc-properties/projects/photos/000/000/:id/:style/:basename.:extension",
+                      :path => "/properties/photos/000/000/:id/:style/:basename.:extension",
+                      :s3_host_name => 's3-us-west-1.amazonaws.com',
+                      :storage => :s3,
+                      :s3_protocol => :https,
+                      :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  	if Rails.env.production?
+    	validates_attachment :photo, #:presence => true,
+                    :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/JPG"] },
+                    :size => { :in => 0..2.megabytes }
+  	elsif Rails.env.test?
+    	do_not_validate_attachment_file_type :photo
+  	else
+    	do_not_validate_attachment_file_type :photo
+  	end
+
+
+	def s3_credentials
+	    {:bucket => Figaro.env.S3_BUCKET, :access_key_id =>  Figaro.env.S3_ACCESS_KEY_ID, :secret_access_key =>  Figaro.env.S3_SECRET_ACCESS_KEY}
+	end
 end
